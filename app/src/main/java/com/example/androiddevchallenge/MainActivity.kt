d3cee259ccb208fbf37c2d975d0a8e84f80220b9
Stephen <stephen.prochnow@gmail.com>
2021-03-01 18:39:17 +0100

    Add list screen for showing adoptable dogs

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,44 +18,168 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.fadeIn
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.GridCells.Fixed
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.androiddevchallenge.R.string
+import com.example.androiddevchallenge.entity.AdoptableDog
+import com.example.androiddevchallenge.entity.BASE_URL_DOG_IMAGES
+import com.example.androiddevchallenge.ui.components.PetCard
+import com.example.androiddevchallenge.ui.sample.Sample
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            MyTheme {
-                MyApp()
-            }
-        }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      MyTheme {
+        DogListScreen()
+      }
     }
+  }
 }
 
-// Start building your app here!
+@OptIn(ExperimentalAnimationApi::class, ExperimentalFoundationApi::class)
 @Composable
-fun MyApp() {
+fun DogListScreen() {
+  val dogsToDisplay = Sample.dogs
+
+  Scaffold {
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+      val gridState = rememberLazyListState()
+      val alpha = listScrollStateToAlpha(gridState)
+      var height by remember { mutableStateOf(Dp.Unspecified) }
+
+      MainHeader(
+          modifier = Modifier
+            .onGloballyPositioned {
+              height = with(LocalDensity.current) { it.size.height / density }.dp
+            }
+            .alpha(alpha)
+      )
+
+      AnimatedVisibility(
+          visible = height != Dp.Unspecified, enter = fadeIn(), initiallyVisible = false
+      ) {
+        PetCardGrid(gridState, height, dogsToDisplay)
+      }
     }
+  }
 }
 
-@Preview("Light Theme", widthDp = 360, heightDp = 640)
+@Composable
+private fun listScrollStateToAlpha(gridState: LazyListState) =
+  remember(gridState.firstVisibleItemScrollOffset, gridState.firstVisibleItemIndex) {
+    if (gridState.firstVisibleItemIndex > 0) {
+      0f
+    } else {
+      1f - (gridState.firstVisibleItemScrollOffset / 100f) / 2
+    }
+  }
+
+@Composable
+private fun MainHeader(
+    modifier: Modifier = Modifier
+) {
+  Surface(
+      modifier = modifier,
+      color = MaterialTheme.colors.primary
+  ) {
+    Column(
+        modifier
+          .fillMaxWidth()
+          .padding(start = 8.dp)
+    ) {
+      Spacer(modifier = Modifier.height(8.dp))
+      Text(
+          text = stringResource(string.mainlist_app_title), style = MaterialTheme.typography.h4,
+      )
+      CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+        Text(
+            text = stringResource(string.main_list_app_subtitle),
+            style = MaterialTheme.typography.subtitle2,
+        )
+      }
+      Spacer(modifier = Modifier.height(8.dp))
+    }
+  }
+}
+
+@OptIn(ExperimentalAnimationApi::class, ExperimentalFoundationApi::class)
+@Composable
+private fun PetCardGrid(
+    gridState: LazyListState,
+    firstRowOffset: Dp,
+    items: List<AdoptableDog>
+) {
+  LazyVerticalGrid(state = gridState, cells = Fixed(2)) {
+    items(count = items.size) { index ->
+      val dog = items[index]
+      val modifier = if (index < 2) {
+        Modifier.padding(top = firstRowOffset + 8.dp)
+      } else {
+        Modifier
+      }
+      PetCard(
+          modifier = modifier,
+          imageData = "$BASE_URL_DOG_IMAGES${dog.id}",
+          title = dog.name,
+          subtitle = "${dog.ageString} - ${dog.sex}"
+      )
+    }
+  }
+}
+
+@Preview
+@Composable
+fun PetCellPreview() {
+  MyTheme {
+    PetCard()
+  }
+}
+
+@Preview
 @Composable
 fun LightPreview() {
-    MyTheme {
-        MyApp()
-    }
+  MyTheme {
+    DogListScreen()
+  }
 }
 
-@Preview("Dark Theme", widthDp = 360, heightDp = 640)
+@Preview
 @Composable
 fun DarkPreview() {
-    MyTheme(darkTheme = true) {
-        MyApp()
-    }
+  MyTheme(darkTheme = true) {
+    DogListScreen()
+  }
 }
